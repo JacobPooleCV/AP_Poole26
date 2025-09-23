fad5cdaa2d9f851b9f9f1020c59bc133318cc214
JacobPooleCV <jpoole@gusd.net>
2025-09-23 14:20:03 -0700

    lab templates 18-31

---
+++ 100_HW/HW5_Colors/base_code/starter.java
@@ -0,0 +1,20 @@
+/*
+ *	Author:
+ *  Date:
+ * 	Collaborator:
+*/
+
+import java.util.*;
+
+class starter {
+	public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the color chooser!");
+        System.out.println("Below you will type in 3 numbers to generate ");
+
+	}
+
+    public static String getColor(int red, int green, int blue){
+        
+    }
+}
