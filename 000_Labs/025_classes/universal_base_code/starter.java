d687ff641673c734403c33469726e40e2426c3c2
JacobPooleCV <jpoole@gusd.net>
2025-10-21 08:28:38 -0700

    E15

--- 000_Labs/025_classes/universal_base_code/starter.java
+++ 000_Labs/025_classes/universal_base_code/starter.java
@@ -3,12 +3,15 @@
  *  Date: 
 */
 
+import pkg.*;
 import java.util.Scanner;
+import java.util.Random;
+
 
 class starter {
 	public static void main(String args[]) {
 		// Your code goes below here
-
+		BaseClass test = new BaseClass();
 
 
 		
