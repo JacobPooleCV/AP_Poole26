f4817e6a44aaef59206d0f767bf92fec32305aa7
JacobPooleCV <jpoole@gusd.net>
2026-01-14 11:10:40 -0800

    E31

---
+++ 102_Exercises/31_Sentences/basecode/starter.java
@@ -0,0 +1,20 @@
+/*
+	Author:
+	Date:
+*/
+import java.util.*;
+
+class starter {
+	public static void main(String args[]) {
+		String[][] words = {
+			// Col 0, 1, 2, 3
+			{"The", "A", "My", "Our"},					// Row 0
+			{"quick", "lazy", "happy", "curious"},		// Row 1
+			{"dog", "cat", "robot", "student"},			// Row 2
+			{"eats", "builds", "chases", "studies"},	// Row 3
+			{"pizza", "code", "homework", "dreams"}		// Row 4
+		};
+		
+
+	}
+}
