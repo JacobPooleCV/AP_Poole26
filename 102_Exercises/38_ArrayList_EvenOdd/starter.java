9a7336234e7f459fa415d0144ec2c74a35ac1831
JacobPooleCV <jpoole@gusd.net>
2026-02-10 11:42:38 -0800

    E38

--- 102_Exercises/38_ArrayList_EvenOdd/starter.java
+++ 102_Exercises/38_ArrayList_EvenOdd/starter.java
@@ -7,13 +7,15 @@ class starter {
 		for(int i = 0; i < 200; i = i + 10){
 			fullSet.add(i);
 		}
+		System.out.println("---------Base ArrayList---------");
 		printArrayList(fullSet);
 		System.out.println();
-		// Create 50 random numbers, place them into the ArrayList given in correct order!
+		// Create 50 random numbers (1-200 inclusive), place them into the ArrayList given in correct order!
 
 
-
+		System.out.println("---------With Random Numbers---------");
 		printArrayList(fullSet);
+		System.out.println();
 		// After your place all numbers in the ArrayList, create 2 separate ArrayLists of all even and odd numbers.
 		ArrayList<Integer> evens = new ArrayList<Integer>();
 		ArrayList<Integer> odds = new ArrayList<Integer>();
@@ -22,8 +24,10 @@ class starter {
 		
 		System.out.println("---------Evens---------");
 		printArrayList(evens);
+		System.out.println();
 		System.out.println("---------Odds---------");
 		printArrayList(odds);
+		System.out.println();
 	}
 
 	public static void printArrayList(ArrayList<Integer> arr){
