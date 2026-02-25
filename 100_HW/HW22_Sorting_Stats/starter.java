b6bc33114d3408cc792121ad49719bd45ee1bf34
JacobPoole <jacob.poole589@gmail.com>
2026-02-25 02:13:58 -0800

    HW22

---
+++ 100_HW/HW22_Sorting_Stats/starter.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+/*
+	Author:
+	Date:
+*/
+
+class starter {
+	public static void main(String args[]) {
+		int[] arr =   {
+			// Place data here!
+        };
+
+		// Make 2 arrays that copy the data given!
+			// Each of the 3 arrays should be called on each sorting algorithm
+		// Call each of the sorting algorithms
+			// Each algorithm should sort, time, count comparisons, and count swaps
+		// In the main, after sorting
+			// Print the largest value
+			// Print the smallest value
+			// Print the size of the array
+
+	}
+
+	public static void bubbleSort(int[] arr){
+
+	}
+
+	public static void selectionSort(int[] arr){
+
+	}
+
+	public static void insertionSort(int[] arr){
+
+	}
+
+    public static void printArray(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+}
